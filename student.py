527a83d67cae2a706d0fcb1c3298669baea35f75
Aniktha-M <ankitham2607@gmail.com>
2025-10-30 16:43:56 +0530

    Create student.py

---
+++ student.py
@@ -0,0 +1,11 @@
+# Student details
+name = input("Enter student's name: ")
+age = int(input("Enter student's age: "))
+usn = input("Enter student's USN: ")
+addr = input("Enter student's address: ")
+
+print("\nStudent Details:")
+print("Name:", name)
+print("Age:", age)
+print("USN:", usn)
+print("Address:", addr)
